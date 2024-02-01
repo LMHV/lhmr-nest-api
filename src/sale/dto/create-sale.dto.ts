5d62f86afd903fd646c0019d21a2195a93a4a2e3
Lucho <luis.herrero.facultad@gmail.com>
2024-02-01 14:38:46 -0300

    UPDATE: dtos

--- src/sale/dto/create-sale.dto.ts
+++ src/sale/dto/create-sale.dto.ts
@@ -1,7 +1,11 @@
 import { JsonObject } from "@prisma/client/runtime/library";
+import { IsString, IsNotEmpty } from 'class-validator'
 
 export class CreateSaleDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly userId: string;
+  @IsNotEmpty()
   readonly products: JsonObject;
 }
 
