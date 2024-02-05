416bebf386138dc160a35031cee3883ff7239a13
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2024-02-05 16:47:43 -0300

    ADD: CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true
   });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 3001);
